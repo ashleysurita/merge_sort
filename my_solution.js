4459882064e565ddcca01f1e85132ef80c3ab2c4
Ashley <ashleywong678@gmail.com>
2021-05-11 10:03:41 -0400

    Update my_solution.js

--- my_solution.js
+++ my_solution.js
@@ -1,50 +1,44 @@
 function mergeSort(array) {
     if(array.length <= 1) return array
     
-    const mid = Math.floor(array.length / 2)
-    const left = []
-    const right = []
+    let mid = Math.floor(array.length / 2)
+    let left = []
+    let right = []
     for(let i = 0; i < array.length; i++){
         if(i < mid) left.push(array[i])
         else right.push(array[i])
     }
-    const arr1 = mergeSort(left)
-    const arr2 = mergeSort(right)
+    
+    let arr1 = mergeSort(left)
+    let arr2 = mergeSort(right)
     
     return merge(arr1, arr2)
 }
 
-function merge(arr1, arr2){
-    let merged = []
+function merge(arr1 = [], arr2 = []){
+    let res = []
     
-    // while both arrays have elements
-    //     compare the smaller element
-    //         the smaller one gets added to our merged array and removed from the array it came from
-    while(arr1.length && arr2.length){
-        if(arr1[0] < arr2[0]){
-            merged.push(arr1[0])
-            arr1.shift()
+    let a1 = 0
+    let a2 = 0
+    while(a1 < arr1.length && a2 < arr2.length){
+        if(arr1[a1] < arr2[a2]){ 
+            res.push(arr1[a1])
+            a1++
         }
-        else {
-            merged.push(arr2[0])
-            arr2.shift()
+        else{
+            res.push(arr2[a2])
+            a2++
         }
     }
+    // console.log(res, a1, a2)
     
-    // if either array still has elements
-    // loop through them and append to end of merged array
-    while(arr1.length){
-        merged.push(arr1.shift())
-    }
-    while(arr2.length){
-        merged.push(arr2.shift())
-    }
+    res = [...res, ...arr1.slice(a1), ...arr2.slice(a2)]
     
-    return merged   
+    return res.flat()
 }
 
 // Test Cases
-// console.log(mergeSort([])) // []
-// console.log(mergeSort([1])) // [1]
+console.log(mergeSort([])) // []
+console.log(mergeSort([1])) // [1]
 console.log(mergeSort([3, 1, 2, 4])) // [1, 2, 3, 4]
 console.log(mergeSort([-10, 1, 3, 8, -13, 32, 9, 5])) // [-13, -10, 1, 3, 5, 8, 9, 32]
